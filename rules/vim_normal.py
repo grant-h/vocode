897e409d9b611f2fbfc7674a29741a6b2bf873fb
Vijay Prakash <vjprakash0@gmail.com>
2017-01-22 01:22:19 -0500

    added few more editing commands

--- rules/vim_normal.py
+++ rules/vim_normal.py
@@ -12,7 +12,7 @@ class VimNormalRules(MappingRule):
 		self.parent = None
 		super(VimNormalRules, self).__init__()
 
-	name = "vim_normal"
+	name = "vim_normal" market is in
 	mapping = {
 		'insert':  Key("i"),
 		'escape':  Key("escape"),
@@ -21,7 +21,7 @@ class VimNormalRules(MappingRule):
 
 		# Buffer management
 		"save" : Key("escape,colon,w,enter"),
-		"quit" : Key("escape,colon,q,enter"),
+		"(quit | exit)" : Key("escape,colon,q,enter"),
 
 		# History
 		"(undo|scratch|whoops)": Key("u"),
@@ -38,8 +38,9 @@ class VimNormalRules(MappingRule):
 		"right" : Key("right"),
 		"[goto] [the] end of [the] line" : Key("dollar"),
 		"[goto] [the] (start|beginning) of [the] line" : Key("caret"),
+		"go to line [number] <line_num>" : Key("escape,colon") + Text("%(line_num)d") + Key("enter"),
 		# TODO: fix conflict using Compound
-		# http://dragonfly.readthedocs.io/en/latest/rules.html#compoundrule-class
+		# http://dragohare Thefly.readthedocs.io/en/latest/rules.html#compoundrule-class
 		#"append" : Key("a"),
 		"append at [the] end of [the] line" : Key("A"),
 		# TODO: fix conflict
@@ -48,7 +49,16 @@ class VimNormalRules(MappingRule):
 		# Editing
 		"delete" : Key("x"),
 		"strike | delete line" : Key("d,d"),
-
+		"select <total_line> line[s] above" : Key("escape,s-v,up:%(total_line)d"),
+		"select <total_line> line[s] below" : Key("escape,s-v,down:%(total_line)d"),
+		"copy <total_line> line[s] above" : Key("escape,V,up:%(total_line)d,y"),
+		"copy <total_line> line[s] below" : Key("escape,V,down:%(total_line)d,y"),
+		"cut <total_line> line[s] above" : Key("escape,V,up:%(total_line)d,d"),
+		"cut <total_line> line[s] below" : Key("escape,V,down:%(total_line)d,d"),
+		"paste": Key("p"),
+		"copy" : Key("y"),
+		"cut" : Key("d"),
+		
 		# Searching
 		"(find | search for) <text>" : Function(do_search),
 		"next" : Key("n"),
@@ -57,8 +67,8 @@ class VimNormalRules(MappingRule):
 		# Movements
 		#"<n> up" : Text ("%(up)s"),
 		}
-	extras = [Dictation("text")]
-	defaults = {"text" : ""}
+	extras = [Dictation("text"), IntegerRef("total_line", 1,100), IntegerRef("line_num", 1, 9999)]
+	defaults = {"text" : "", "total_line": 1, "line_num": 1}
 
 	def _process_recognition(self, value, extras):
 		#print "REC", str(self), "VALUE", str(value), "EXTRAS", str(extras)
