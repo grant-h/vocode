c8a5103f0f155b2bb24323e25d8945ab39a94f4f
Grant Hernandez <grant.h.hernandez@gmail.com>
2017-01-22 00:45:31 -0500

    Python variable formatting

--- knowledge/python.py
+++ knowledge/python.py
@@ -12,10 +12,43 @@ def format_camel(text):
 			output += w
 	return output
 
-def fmtCamel(text):
+def format_default(text):
+	"""
+	Format the sentence divided by spaces in to default_spacing
+	"""
+	output = []
+	for i,w in enumerate(text.split(" ")):
+		output += [w.lower()]
+	return "_".join(output)
+
+def format_class(text):
+	"""
+	Format the sentence divided by spaces in to ClassCasing
+	"""
+	output = []
+	for i,w in enumerate(text.split(" ")):
+		output += [w[0].upper() + w[1:]]
+	return "".join(output)
+
+
+# default - this_is_a_variable
+# camel   - thisIsAVariable
+# class   - ThisIsAVariable
+
+def fmtVarCamel(text):
 	text = str(text)
 
-	Text("%s = VAR" % format_camel(text)).execute()
+	Text("%s" % format_camel(text)).execute()
+
+def fmtVarDefault(text):
+	text = str(text)
+
+	Text("%s" % format_default(text)).execute()
+
+def fmtVarClass(text):
+	text = str(text)
+
+	Text("%s" % format_class(text)).execute()
 
 def defun(text):
 	text = str(text)
@@ -30,11 +63,27 @@ class PythonRules(MappingRule):
 	name = "python_lang"
 	mapping = {
 		"[new] import <text>" : Function(lang_import),
-		"defun <text>" : Function(defun)
+		"defun <text>" : Function(defun),
+
+		# Writing variables
+		"var camel <text>" : Function(fmtVarCamel),
+		"var default <text>" : Function(fmtVarDefault),
+		"var class <text>" : Function(fmtVarClass),
+		"assign to" : Text(" = "),
+
+		# Python version of None
+		"none" : Text("None"),
+		"false" : Text("False"),
+		"true" : Text("True"),
+
+		# Python types
+		"whomp (array | access)" : Text("[]") + Key("left"),
+		"whomp (lookup|dictionary)" : Text("dict()"),
+		"whomp set" : Text("set()")
 		}
 	extras = [Dictation("text")]
 	defaults = {}
 
 	def _process_recognition(self, value, extras):
-		#print("PyLang: %s" % value._action._spec)
+		print("PY: %s" % str(value._action))
 		value.execute(extras)
