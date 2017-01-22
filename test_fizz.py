79de950c59781ba41a3d6b2e47358b57af5cf626
Grant Hernandez <grant.h.hernandez@gmail.com>
2017-01-22 10:54:08 -0500

    Add fizz buzz testscript

---
+++ test_fizz.py
@@ -0,0 +1,13 @@
+# for i in range(1, 100):
+#     output = "" 
+# 
+#     if i % 3 == 0:
+#       output += "Fizz"
+# 
+#     if i % 5 == 0:
+#       output += "Buzz"
+# 
+#     if  output== "":
+#       output = str(i)
+# 
+#     print(output)
