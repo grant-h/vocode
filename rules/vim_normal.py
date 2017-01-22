543aacfba133e8ca53c73a09152f5b7bec548625
Grant Hernandez <grant.h.hernandez@gmail.com>
2017-01-21 22:22:21 -0500

    Reimport normal mode commands from old.py

--- rules/vim_normal.py
+++ rules/vim_normal.py
@@ -1,4 +1,11 @@
+import dragonfly
 from dragonfly import *
+
+def do_search(text):
+	text = str(text)
+	print("Searching for '%s'" % text)
+	act = Key("slash") + Text(text)
+	act.execute()
 
 class VimNormalRules(MappingRule):
 	def __init__(self):
@@ -10,28 +17,57 @@ class VimNormalRules(MappingRule):
 		'insert':  Key("i"),
 		'escape':  Key("escape"),
 		'grab all [text]' : Key("g, g, V, G"),
+		"enter|return" : Key("enter"),
+
+		# Buffer management
+		"save" : Key("escape,colon,w,enter"),
+		"quit" : Key("escape,colon,q,enter"),
 
 		# History
 		"(undo|scratch|whoops)": Key("u"),
 		"redo": Key("c-r"),
 
 		# Movement
+		"pageup" : Key("pgup"),
+		"pagedown": Key("pgdown"),
 		"bottom" : Key("G"),
-		"top" : Key("g,g")
+		"top" : Key("g,g"),
+		"up" : Key ("up"),
+		"down" : Key ("down"),
+		"left" : Key("left"),
+		"right" : Key("right"),
+		"[goto] [the] end of [the] line" : Key("dollar"),
+		"[goto] [the] (start|beginning) of [the] line" : Key("caret"),
+		# TODO: fix conflict using Compound
+		# http://dragonfly.readthedocs.io/en/latest/rules.html#compoundrule-class
+		#"append" : Key("a"),
+		"append at [the] end of [the] line" : Key("A"),
+		# TODO: fix conflict
+		#"insert at [the] (start|beginning) of [the] line" : Key("I"),
+
+		# Editing
+		"delete" : Key("x"),
+		"strike | delete line" : Key("d,d"),
+
+		# Searching
+		"(find | search for) <text>" : Function(do_search),
+		"next" : Key("n"),
+		"clear search" : Text(":nohl") + Key("enter")
 
 		# Movements
 		#"<n> up" : Text ("%(up)s"),
 		}
-	extras = []
-	defaults = {}
+	extras = [Dictation("text")]
+	defaults = {"text" : ""}
 
 	def _process_recognition(self, value, extras):
 		#print "REC", str(self), "VALUE", str(value), "EXTRAS", str(extras)
 		#print "action", value._action, "data", value._data
-		print("NORM: %s" % value._action._spec)
-		if value._action._spec == "i":
+		action = value._action
+		print("NORM: %s" % str(action))
+
+		# Nasty hack
+		if isinstance(action, dragonfly.actions.action_key.Key) and action._spec in ["i", "a", "A", "I", "o", "O"]:
 			self.parent.handle_event("insert")
 
 		value.execute(extras)
-		#self.process_recognition(value)
-
